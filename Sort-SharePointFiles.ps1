37b8b99b0f7883546bf8dfaab4488917efdf12b2
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2020-05-27 15:23:20 -0400

    Add files via upload

---
+++ Sort-SharePointFiles.ps1
@@ -0,0 +1,91 @@
+﻿<## -- Sort-SharePointFiles --
+
+This script moves files from the unsorted directory in the new service library
+to the final location in the service library. It identifies the correct work 
+number, looks that up against SQL, pulls job information from SQL about that 
+job, and sorts the file with that information.
+
+This script also tests if the files already exist. If not, the file tree is 
+created before the file is put into place.
+
+#>
+
+# Declare the standard variables. These assume that the script is run from a computer with the libraries 
+# synchronized.
+$SourcePath = "~\Noyes Air Conditioning\Service - GoCanvas work reports (unsorted)"
+$DestinationPath = "~\Noyes Air Conditioning\Service - Technicians"
+
+
+# Pull the list of current work orders and job numbers from the Spectrum database.
+# $query = @"
+#     SELECT * 
+#     FROM [dbo].[Z_EKTO_REFERENCE_MERGED_JOBS_AND_WORK_ORDERS]
+# "@
+# $results = Invoke-Sqlcmd -Query $query -ServerInstance "spectrum.nacgroup.com" -Database "Forefront"
+
+
+
+# Pull all file names for GoCanvas reports into a variable. This again assumes the script is run 
+# from a computer synchronized with document library holding unsorted GoCanvas reports.
+$files = Get-ChildItem -Path $SourcePath
+$WorkNumbers = @()
+$WorkSites = @()
+$WorkReports = @()
+
+# Processing each file.
+ForEach ($f in $files) {
+    # Read the file name and split it into various reference variables.
+    $WorkNumber = $f.Name.Split("-")[0]
+    $WorkNumbers += $WorkNumber
+    $WorkSite = $f.Name.Split("-")[1]
+    $WorkSites += $WorkSite
+    $workReport = $f.Name.Split("-")[2]
+    $WorkReports += $WorkReport
+    # Using the work order or job number find the corresponding information from Spectrum.
+    $query = @"
+        SELECT * 
+        FROM [dbo].[Z_EKTO_REFERENCE_MERGED_JOBS_AND_WORK_ORDERS]
+        WHERE Job_Number = '$WorkNumber'
+"@
+    $result = Invoke-Sqlcmd -Query $query -ServerInstance "spectrum.nacgroup.com" -Database "Forefront"
+    # User the resulting data to get the identifier for the site directory.
+    $SiteCode = $result.Site_Code
+    # Search for the site directory.
+    if (Test-Path "$DestinationPath\*$SiteCode") {
+        # If the search is successful save the absolute path of the site directory.
+        $Path = Get-ChildItem -Path "$DestinationPath\*$SiteCode"
+        if ($WorkNumber.Length -eq 10) {
+            # Reports with 10-digit job numbers go directly to the associated job folder.
+            if (Test-Path "$Path\Quoted projects\$WorkNumber*") {
+                Copy-Item "$SourcePath\$f" -Destination "$Path\Quoted projects\$WorkNumber*"
+                Write-Host "I moved $f.Name to $Path\Quoted projects\$WorkNumber."
+                }
+            else {
+                # If no job folder exists create one and move the report.
+                $WorkDescription = $result.Description
+                New-Item -Type Directory -Path "$Path\Quoted projects\" -Name "$WorkNumber - $WorkDescription"
+                Write-Host "I created a new directory ($WorkNumber - $WorkDescription)"
+                Copy-Item "$SourcePath\$f" -Destination "$Path\Quoted projects\$WorkNumber*"
+                Write-Host "I moved $f.Name to $Path\Quoted projects\$WorkNumber."
+                }
+            }
+        if ($WorkNumber.Length -eq 5) {
+            # Reports with 5-digit job numbers can land in several different directories based on the 
+            # report type.
+            if ($WorkReport -like "Work Order*") {$ReportDir = "Work order reports"}
+            elseif ($WorkReport -like "Scheduled Maintenance*") {$ReportDir = "Scheduled maintenance reports"}
+            elseif ($WorkReport -like "Scheduled PM*") {$ReportDir = "Scheduled maintenance reports"}
+            elseif ($WorkReport -like "Chiller Service*") {$ReportDir = "Scheduled maintenance reports"}
+            elseif ($WorkReport -like "Chiller Overhaul*") {$ReportDir = "Scheduled maintenance reports"}
+            elseif ($WorkReport -like "Plant Engineering*") {$ReportDir = "Scheduled maintenance reports"}
+            else {$ReportDir = "Scheduled maintenance reports"}
+            Copy-Item "$SourcePath\$f" -Destination "$Path\$ReportDir"
+            if (Test-Path "$Path\$ReportDir\$f") { Move-Item "$SourcePath\$f" -Destination "$SourcePath\Sorted" }
+            Write-Host "I moved $f to $Path\$ReportDir."
+            }
+        else {
+            # Error handling if job number isn't 5- or 10-digits long.
+            Write-Host "I can't write $f anywhere."
+            }
+        }
+    }
---
+++ Update-ServiceDirectories.ps1
@@ -0,0 +1,110 @@
+﻿<## -- Update-ServiceDirectories --
+
+This script reviews each of the directories in the Service - Technicians 
+document library with the intent of moving directories for sites tagged
+as inactive or unused as well as creating new site directories, as 
+necessary.
+
+#>
+
+# Declare the standard variables. These assume that the script is run from a computer with the libraries 
+# synchronized.
+$PathStatusA = "~\Noyes Air Conditioning\Service - Technicians"
+$PathStatusI = "~\Noyes Air Conditioning\Service - Technicians\Inactive sites"
+$PathStatusU = "~\Noyes Air Conditioning\Service - Technicians\Unused sites"
+$SiteCodes = @()
+$directories = @()
+$directories = Get-ChildItem -Path $PathStatusA
+$directories += Get-ChildItem -Path $PathStatusI
+$directories += Get-ChildItem -Path $PathStatusU
+
+# Begin direcotry processing.
+ForEach ($d in $directories) {
+    # Isolate site codes.
+    $SiteCode = $d.Name.Split("-").Trim()
+    $SiteCode = $SiteCode[1]
+    $SiteCodes += $SiteCode
+    # Determine site status with SQL query.
+    $query = @"
+        SELECT *
+        FROM [dbo].[Z_EKTO_REFERENCE_SITE_MASTER]
+        WHERE Site_Code = '$SiteCode'
+"@
+    $result = Invoke-Sqlcmd -Query $query -ServerInstance "spectrum.nacgroup.com" -Database "Forefront"
+    $SiteStatus = $result.Status
+
+    # Conditional checks begin.
+    if ($SiteStatus -match "A") {
+        # For active sites: search inactive and unused dirs for folders. If 
+        # present, move the existing folder, else create a new folder tree.
+        if (Test-Path "$PathStatusA\*$SiteCode") { Write-Host "The directory for $SiteCode already exists." }
+        elseif (Test-Path "$PathStatusI\*$SiteCode") { Move-Item -Path "$PathStatusI\*$SiteCode" -Destination "$PathStatusA\" }
+        elseif (Test-Path "$PathStatusU\*$SiteCode") { Move-Item -Path "$PathStatusU\*$SiteCode" -Destination "$PathStatusA\" }
+        else { 
+            # Make-ServiceDirectories.ps1
+            $SiteDir = $result.Site_Name.Trim() + " - " + $result.Site_Code.Trim()
+            Write-Host "Creating directory tree for $SiteDir."
+            New-Item -Type Directory -Name $SiteDir -Path "$PathStatusA"
+			New-Item -Type Directory -Name "Quoted projects" -Path "$PathStatusA\$SiteDir"
+			New-Item -Type Directory -Name "Scheduled maintenance reports" -Path "$PathStatusA\$SiteDir"
+			New-Item -Type Directory -Name "Service contract" -Path "$PathStatusA\$SiteDir"
+			New-Item -Type Directory -Name "Work order reports" -Path "$PathStatusA\$SiteDir"
+            if (Test-Path "$PathStatusA\$SiteDir\Quoted projects") { 
+                New-Item -Type Directory -Name "Daily job site reports" -Path "$PathStatusA\$SiteDir\Quoted projects"
+                }
+            else {
+                Start-Sleep -Seconds 3
+                New-Item -Type Directory -Name "Daily job site reports" -Path "$PathStatusA\$SiteDir\Quoted projects"
+                }
+            }
+        }
+    elseif ($SiteStatus -match "I") {
+        # For inactive sites.
+        if (Test-Path "$PathStatusA\*$SiteCode") { Move-Item -Path "$PathStatusA\*$SiteCode" -Destination "$PathStatusI" }
+        elseif (Test-Path "$PathStatusI\*$SiteCode") { Write-Host "The directory for $SiteCode already exists." }
+        elseif (Test-Path "$PathStatusU\*$SiteCode") { Move-Item -Path "$PathStatusU\*$SiteCode" -Destination "$PathStatusI" }
+        else { # Make-ServiceDirectories.ps1
+            $SiteDir = $result.Site_Name.Trim() + " - " + $result.Site_Code.Trim()
+            Write-Host "Creating directory tree for $SiteDir."
+            New-Item -Type Directory -Name $SiteDir -Path "$PathStatusI"
+			New-Item -Type Directory -Name "Quoted projects" -Path "$PathStatusI\$SiteDir"
+			New-Item -Type Directory -Name "Scheduled maintenance reports" -Path "$PathStatusI\$SiteDir"
+			New-Item -Type Directory -Name "Service contract" -Path "$PathStatusI\$SiteDir"
+			New-Item -Type Directory -Name "Work order reports" -Path "$PathStatusI\$SiteDir"
+            if (Test-Path "$PathStatusI\$SiteDir\Quoted projects") { 
+                New-Item -Type Directory -Name "Daily job site reports" -Path "$PathStatusI\$SiteDir\Quoted projects"
+                }
+            else {
+                Start-Sleep -Seconds 3
+                New-Item -Type Directory -Name "Daily job site reports" -Path "$PathStatusI\$SiteDir\Quoted projects"
+                }
+            }
+        }
+    elseif ($SiteStatus -match "U") {
+        # For unused sites.
+        if (Test-Path "$PathStatusA\*$SiteCode") { Move-Item -Path "$PathStatusA\*$SiteCode" -Destination "$PathStatusU" }
+        elseif (Test-Path "$PathStatusI\*$SiteCode") { Move-Item -Path "$PathStatusI\*$SiteCode" -Destination "$PathStatusU" }
+        elseif (Test-Path "$PathStatusU\*$SiteCode") { Write-Host "The directory for $SiteCode already exists." }
+        else { # Make-ServiceDirectories.ps1
+            $SiteDir = $result.Site_Name.Trim() + " - " + $result.Site_Code.Trim()
+            Write-Host "Creating directory tree for $SiteDir."
+            New-Item -Type Directory -Name $SiteDir -Path $PathStatusU
+			New-Item -Type Directory -Name "Quoted projects" -Path "$PathStatusU\$SiteDir"
+			New-Item -Type Directory -Name "Scheduled maintenance reports" -Path "$PathStatusU\$SiteDir"
+			New-Item -Type Directory -Name "Service contract" -Path "$PathStatusU\$SiteDir"
+			New-Item -Type Directory -Name "Work order reports" -Path "$PathStatusU\$SiteDir"
+            if (Test-Path "$PathStatusU\$SiteDir\Quoted projects") { 
+                New-Item -Type Directory -Name "Daily job site reports" -Path "$PathStatusU\$SiteDir\Quoted projects"
+                }
+            else {
+                Start-Sleep -Seconds 3
+                New-Item -Type Directory -Name "Daily job site reports" -Path "$PathStatusU\$SiteDir\Quoted projects"
+                }
+            }
+        }
+    else {
+        # Error handling.
+
+        }
+
+    }
