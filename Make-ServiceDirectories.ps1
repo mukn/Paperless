64557d9eff726daf746b32d8243d5821551d1cb0
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2020-05-27 14:30:48 -0400

    Add files via upload

---
+++ Make-ServiceDirectories.ps1
@@ -0,0 +1,86 @@
+<## -- Make-ServiceDirectories --
+
+This script is used to create service directories for each site in Spectrum. It
+sorts based on whether the site is active, inactive, or unused but does not check
+existing directories beforehand.
+
+Reviewed 26 May 2020.
+
+#>
+
+## Functions
+# Remove-SpecialChars - https://stackoverflow.com/q/30778098
+function Remove-SpecialChars {
+	param(
+		[string]$InputString,
+		[string]$Replacement = "",
+		[string]$SpecialChars = "!#%&*<>?/\|{}"
+	)
+	$rePattern = ($specialChars.ToCharArray() | ForEach-Object { [regex]::Escape($_) }) -join "|"
+	$inputString -replace $rePattern,$replacement
+	}
+
+# New-SharePointFolders
+Function New-SharePointFolders {
+	Param(
+		[array]$InputDataTable,
+		[string]$Path
+	)
+	$resultsArray = @()
+	ForEach ($row in $InputDataTable) {
+		$resultsArray += $row
+	}
+    $rootDirectory = Get-Location
+	Set-Location -Path $Path
+	$i = 0
+	$currentDirs = Get-ChildItem -Path .\ | Select-Object -ExpandProperty Name
+	ForEach ($item in $resultsArray.Site_Code) {
+		# Remove blank space from Site_Name & Site_Code
+		$name = $resultsArray[$i].Site_Name.Trim()
+		$name = Remove-SpecialChars -InputString $name
+		$code = $resultsArray[$i].Site_Code.Trim()
+		$dir = "$name - $code"
+		If ($currentDirs -match $code) {
+			Write-Host "Match found in directory $dir."
+		}
+		Else {
+			# Create directories
+			New-Item -Type Directory -Name $dir
+			New-Item -Type Directory -Name "Quoted projects" -Path $dir
+			New-Item -Type Directory -Name "Scheduled maintenance reports" -Path $dir
+			New-Item -Type Directory -Name "Service contract" -Path $dir
+			New-Item -Type Directory -Name "Work order reports" -Path $dir
+			New-Item -Type Directory -Name "Daily job site reports" -Path "$dir\Quoted projects"
+		}
+		$i++
+	}
+	Set-Location -Path $rootDirectory
+}
+
+## 
+# Pull active sites from SQL and save to variable.
+$query = @"
+	SELECT * FROM [dbo].[Z_PAPERLESS_REFERENCE_SITE_MASTER]
+	WHERE Status = 'A'
+	ORDER BY Site_Code
+"@
+$results = Invoke-Sqlcmd -Query $query -ServerInstance spectrum.nacgroup.com -Database Forefront
+New-SharePointFolders -InputDataTable $results -Path .\
+
+# Pull inactive sites from SQL and save to variable.
+$query = @"
+	SELECT * FROM [dbo].[Z_PAPERLESS_REFERENCE_SITE_MASTER]
+	WHERE Status = 'I'
+	ORDER BY Site_Code
+"@
+$results = Invoke-Sqlcmd -Query $query -ServerInstance spectrum.nacgroup.com -Database Forefront
+New-SharePointFolders -InputDataTable $results -Path ".\Inactive sites"
+
+# Pull unused sites from SQL and save to variable.
+$query = @"
+	SELECT * FROM [dbo].[Z_PAPERLESS_REFERENCE_SITE_MASTER]
+	WHERE Status = 'N'
+	ORDER BY Site_Code
+"@
+$results = Invoke-Sqlcmd -Query $query -ServerInstance spectrum.nacgroup.com -Database Forefront
+New-SharePointFolders -InputDataTable $results -Path ".\Unused sites"
---
+++ Move-DuplicatesInSharePoint.ps1
@@ -0,0 +1,45 @@
+﻿<## -- Move-DuplicatesInSharePoint --
+
+This script is used to move files from the old service technician library to
+the new one. This is a one-off script and should not be run again unless the 
+new library is deleted.
+
+Updated 26 May 2020.
+
+#>
+
+## Functions
+
+## Declared variables
+$NewLibraryUri = "https://nacgroup.sharepoint.com/Sites/Service9/Shared%20Documents/Technicians/"
+$NewLibraryPath = "~\Noyes Air Conditioning\Service - Technicians"
+$OldLibraryUri1 = "https://nacgroup.sharepoint.com/Projects%200C/"
+$OldLibraryPath1 = "~\Noyes Air Conditioning\Noyes Air Conditioning - Projects (0-C)"
+$OldLibraryUri2 = "https://nacgroup.sharepoint.com/Projects%20CN/"
+$OldLibraryPath2 = "~\Noyes Air Conditioning\Noyes Air Conditioning - Projects (D-N)"
+$OldLibraryUri3 = "https://nacgroup.sharepoint.com/Projects%20OZ/"
+$OldLibraryPath3 = "~\Noyes Air Conditioning\Noyes Air Conditioning - Projects (O-Z)"
+
+Set-Location -Path $NewLibraryPath
+$dirsService = Get-ChildItem | Select-Object -ExpandProperty Name
+Set-Location -Path $OldLibraryPath3
+$dirsProjects = Get-ChildItem | Select-Object -ExpandProperty Name
+
+$matchCounter = 0
+$matchList = @()
+$mismatchList = @()
+ForEach ($sd in $dirsService) {
+    $name = $sd.Split("-")[0].Trim()
+    if ($name | ?{$dirsProjects -match $_}) {
+        Write-Host "Match found for $sd."
+        New-Item -ItemType File -Name "Updated-$(Get-Date -Format dd-MM-yyyy).txt" -Path "$NewLibraryPath\$sd\"
+        $matchCounter++
+        $matchList += $sd
+        $dirContents = Get-ChildItem "$OldLibraryPath3\$name" -Recurse -Exclude "Quoted Projects"
+        Copy-Item -Path $OldLibraryPath3\$name\* -Destination "$NewLibraryPath\$sd\" -Recurse -Force
+        }
+    else {
+        $mismatch += $sd
+        $mismatchList += $sd
+        }
+}
